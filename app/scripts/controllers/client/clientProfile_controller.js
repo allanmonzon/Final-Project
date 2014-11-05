e5fe1311976ab535d465fe1c380ed37b62e35e85
Allan Monzon <allanjmonzon@gmail.com>
2014-11-04 23:03:38 -0500

    adds client profile

--- app/scripts/controllers/client/clientProfile_controller.js
+++ app/scripts/controllers/client/clientProfile_controller.js
@@ -1,20 +1,24 @@
-Final.ClientProfileController = Ember.ArrayController.extend({
+Final.ClientProfileController = Ember.Controller.extend({
   needs: ['application'],
 
   actions: {
 		createProfile: function(){
-			var profile = this.store.createRecord('ClientProfile', {
+			var profile = this.store.createRecord('clientProfile', {
 				name: this.get('name'),
 				info: this.get('info'),
 				email: this.get('email'),
 				number: this.get('number')
 			});
 
-			profile.save();
-			this.set('name', '');
-			this.set('info', '');
-			this.set('email', '');
-			this.set('number', '');
+			var user = this.get('controllers.application.currentUser');
+			user.set('profile', profile);
+			user.save();
+
+			// profile.save();
+			// this.set('name', '');
+			// this.set('info', '');
+			// this.set('email', '');
+			// this.set('number', '');
 		},
 
     logOut: function(){
--- app/scripts/models/profile_freelancer_model.js
+++ app/scripts/models/profile_freelancer_model.js
@@ -1,3 +1,8 @@
-Final.Profile = DS.Model.extend({
-  freelancer: DS.belongsTo('freelancer')
-});
+Final.Profile = DS.Model.extend({});
+
+Final.ClientProfile = Final.Profile.extend({
+	name: DS.attr('string'),
+	info: DS.attr('string'),
+	email: DS.attr('string'),
+	number: DS.attr('string')
+})
