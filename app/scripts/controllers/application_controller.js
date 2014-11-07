5f90d5a48bf0999375d1a85573c8696eed3cbde1
Jeff-Coons <Jeffcoons@live.com>
2014-11-07 16:29:47 -0500

    Broken Code

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -10,29 +10,26 @@ Final.ApplicationController = Ember.Controller.extend({
     });
   },
 
-  configureSession: function(authData) {
+  authWithToken: function() {
     var self = this;
-    return new Ember.RSVP.Promise(function(resolve, reject){
-      localStorage.setItem('userAuth', authData.token);
-      self.store.find('user', authData.uid).then(function(user){
-        self.set('currentUser', user);
-        resolve(user);
-      }, function(error){
-        var user = self.store.recordForId('user', authData.uid);
-        user.loadedData();
-        self.set('currentUser', user);
-        resolve(user);
+    if (localStorage.getItem('userAuth')) {
+      var localAuthData = JSON.parse(localStorage.getItem('userAuth'));
+      Final.ref.authWithCustomToken(localAuthData.token, function(error, authData) {
+        if (!error) {
+          self.store.find('user', authData.uid).then(function(user) {
+            self.set('currentUser', user);
+            resolve(user)
+          }, function(error) {
+            var user = self.store.recordForId('user', authData.uid);
+            user.loadedData();
+            self.set('currentUser', user);
+            resolve(user);
+          });
+        } else {
+          console.log('Error authenticating user:', error);
+        }
       });
-    });
-  },
-
-  authWithToken: function(token) {
-    var self = this;
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      Final.ref.authWithCustomToken(token, function(error, authData) {
-        self.configureSession(authData).then(resolve, reject);
-      });
-    });
-  },
+    }
+  }
 
 });
