5edaf7613316c1e3ac16d1e12b7139b0f7834f7a
Jeff-Coons <Jeffcoons@live.com>
2014-11-07 15:40:06 -0500

    Initializer Removed

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -7,34 +7,3 @@ Final.ref = new Firebase('https://final-project-1.firebaseio.com/');
 Final.ApplicationAdapter = DS.FirebaseAdapter.extend({
 	firebase: Final.ref
 });
-
-
-Final.initializer({
-	name: "preload",
-
-	initialize: function(container) {
-		Final.deferReadiness();
-
-		if (localStorage.getItem('userAuth')) {
-
-			var localAuthData = JSON.parse(localStorage.getItem('userAuth'));
-			Final.ref.authWithCustomToken(localAuthData.token, function(error, authData) {
-				if (!error) {
-
-					var store = container.lookup('store:main');
-					var appController = container.lookup('controller:application');
-
-				  store.find('user', authData.uid).then(function(user) {
-						appController.set('currentUser', user);
-						Final.advanceReadiness();
-					});
-
-				} else {
-
-					console.log('Error authenticating user:', error);
-
-				}
-			});
-		}
-	}
-});
