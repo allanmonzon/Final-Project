2a85520bec9c01a2d9de475f5cfe030086a8925e
Allan Monzon <allanjmonzon@gmail.com>
2014-11-10 12:07:48 -0500

    adds client profile

---
+++ app/scripts/controllers/client/client-profile_my.js
@@ -0,0 +1,12 @@
+Final.ClientProfileMyController = Ember.ObjectController.extend({
+ needs: ['application'],
+
+ // actions: {
+ //   logOut: function(){
+ //     this.set('currentUser', null);
+ //     localStorage.removeItem('userAuth');
+ //     Final.ref.unauth();
+ //     this.transitionToRoute('index');
+ //   }
+ // }
+});
