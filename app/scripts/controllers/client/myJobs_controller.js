6a957042976d2367620ffc5da8f2e2f83660a469
Allan Monzon <allanjmonzon@gmail.com>
2014-11-12 12:09:31 -0500

    adds Home buttons

--- app/scripts/controllers/client/myJobs_controller.js
+++ app/scripts/controllers/client/myJobs_controller.js
@@ -1,13 +1,6 @@
 Final.MyJobsController = Ember.ArrayController.extend({
-	needs: ['application', 'bid'],
-	user: Ember.computed.alias('controllers.application.currentUser'),
-	//itemController: 'bid',
-
-});
-
-Final.BidController = Ember.ArrayController.extend({
 	needs: ['application'],
-
-
+	user: Ember.computed.alias('controllers.application.currentUser'),
 
 });
+
