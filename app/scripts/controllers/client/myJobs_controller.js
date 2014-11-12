7283998030c694378cef97331d756217afc5499b
Jeff-Coons <Jeffcoons@live.com>
2014-11-12 12:08:55 -0500

    Links have been inserted in most pages

--- app/scripts/controllers/client/myJobs_controller.js
+++ app/scripts/controllers/client/myJobs_controller.js
@@ -1,13 +1,15 @@
 Final.MyJobsController = Ember.ArrayController.extend({
-	needs: ['application', 'bid'],
-	user: Ember.computed.alias('controllers.application.currentUser'),
-	//itemController: 'bid',
-
-});
-
-Final.BidController = Ember.ArrayController.extend({
 	needs: ['application'],
+	user: Ember.computed.alias('controllers.application.currentUser'),
 
+	// Need to each over the bids associated to the specific job
 
+	actions: {
+
+		acceptBid: function() {
+			console.log('cool');
+		}
+
+	}
 
 });
